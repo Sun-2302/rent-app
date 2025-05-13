0772fb6115d0ba4dc1ae99745436bf85d37de746
Sun-2302 <hei.dera.932@gmail.com>
2025-05-13 11:15:16 +0300

    feat: add rent.js

---
+++ rent.js
@@ -0,0 +1,30 @@
+class RentableItem {
+    constructor(name) {
+        this.name = name;
+        this.availability = true;
+    }
+    
+    rent(){
+        if (!this.availability){
+            return `!! ${this.name} is not available !!`;
+        }
+        this.availability = false;
+        return `${this.name} has been rented successfully.`;
+    }
+
+    returnItem(){
+        this.availability = true;
+        return `${this.name} has been returned and is now available.`
+    }
+}
+
+
+//Test avec voiture
+const car = new RentableItem("Car");
+
+console.log(car.rent());
+console.log(car.rent());
+
+console.log(car.returnItem());
+console.log(car.rent());
+
